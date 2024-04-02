5288cf6df3ed950d8c3e2d64ce9ef180b440592d
alvimm <filipe2012alvim@gmail.com>
2024-04-02 14:57:25 -0300

    Refactor travel packages screen and custom search bar

--- lib/screens/travel_packages_screen.dart
+++ lib/screens/travel_packages_screen.dart
@@ -1,17 +1,120 @@
+// ignore_for_file: library_private_types_in_public_api
+import 'package:explore_mundo_app/utils/colors.dart';
+import 'package:explore_mundo_app/utils/data.dart';
+import 'package:explore_mundo_app/widgets/custom_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class TravelPackagesScreen extends StatelessWidget {
+class TravelPackagesScreen extends StatefulWidget {
   const TravelPackagesScreen({super.key});
 
   @override
+  _TravelPackagesScreenState createState() => _TravelPackagesScreenState();
+}
+
+class _TravelPackagesScreenState extends State<TravelPackagesScreen> {
+  String _searchTerm = '';
+  
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Pacotes',
-        style: TextStyle(
-          fontSize: 24,
+        final filteredData = dataPlaces
+        .where((place) => place.title.toLowerCase().contains(_searchTerm.toLowerCase()))
+        .toList();
+
+    return Column(
+      children: [
+        CustomSearchBar(
+          onSearch: (search) {
+            setState(() {
+              _searchTerm = search;
+            });
+          },
         ),
-      ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: filteredData.length,
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.white54,
+                margin: const EdgeInsets.all(20),
+                child: Row(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      ),
+                      child: Image.asset(
+                        filteredData[index].image,
+                        fit: BoxFit.cover,
+                        height: 200,
+                        width: 150,
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Text(filteredData[index].title,
+                                  style: const TextStyle(fontSize: 20)),
+                              Text(filteredData[index].from,
+                                  style: const TextStyle(fontSize: 15)),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 80, right: 10, left: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: filteredData[index].rating <= 75
+                                          ? Colors.red[500]
+                                          : Colors.yellow[500],
+                                    ),
+                                    Text(filteredData[index].rating.toString()),
+                                  ],
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    children: <TextSpan>[
+                                      TextSpan(
+                                        text: 'R\$ ',
+                                        style: GoogleFonts.urbanist(
+                                          color: MyColors.primary,
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.w800,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: '${filteredData[index].price}',
+                                        style: GoogleFonts.urbanist(
+                                          color: MyColors.primary,
+                                          fontSize: 40,
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/widgets/custom_search_bar.dart
+++ lib/widgets/custom_search_bar.dart
@@ -1,16 +1,35 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:explore_mundo_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 
-class CustomSearchBar extends StatelessWidget {
-  const CustomSearchBar({super.key});
+class CustomSearchBar extends StatefulWidget {
+  final ValueChanged<String> onSearch;
+  const CustomSearchBar({super.key, required this.onSearch});
+
+  @override
+  _CustomSearchBarState createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      widget.onSearch(_controller.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 10, bottom: 30),
+      padding: const EdgeInsets.all(10),
       child: SearchBar(
+        controller: _controller,
         textCapitalization: TextCapitalization.words,
-        hintText: 'Pesquisar Destinos',
+        hintText: 'Pesquisar pacotes',
         hintStyle: MaterialStateProperty.all(
           const TextStyle(
             color: MyColors.hintText,
